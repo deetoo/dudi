89fdaac5cba7d89d1c6f96b508658ecc13b8cdd9
Doug Dobies <doug.dobies@gmail.com>
2013-10-27 03:53:24 -0400

    new updates

--- dudi.sh
+++ dudi.sh
@@ -2,6 +2,7 @@
 
 USERFILE="/home/www-data//user.dat"
 SERVERSFILE="/home/www-data/servers.dat"
+RUNLOG="/home/www-data/log"
 
 if [ -f $USERFILE ]
 	then
@@ -9,13 +10,14 @@ DelUser=`cat $USERFILE`
 
 if [ $DelUser = "root" ]
 	then
-		echo "Admin user may not be disabled."
+		echo "Admin user may not be disabled." >$RUNLOG
 		exit
 	fi
 
 while read server
 	do
-		 ssh root@$server "usermod -L -e 1 $DelUser"
+         echo "Account: $DelUser disabled at: `date`" >$RUNLOG
+		 ssh root@$server "usermod -L -e 1 $DelUser" 
 	done < $SERVERSFILE
 rm $USERFILE
 
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
+<title>DUDI - Distributed Useraccount Disabler</title>
+<body bgcolor="ffffff" text="#00000" link="#ff0000" vlink="#ff0000">
 <?
+
 error_reporting(E_ALL);
 
 
@@ -7,7 +10,7 @@ $DelFile = "/home/www-data/user.dat";
 
 
 if (file_exists($DelFile)) {
-    echo "<center>There is an account already in the queue to be disabled.<br>Please try again in five minutes.</center></html>";
+    echo "<center><img src=\"dudi.png\" border=\"0\"><br>There is an account already in the queue to be disabled.<br>Please try again in five minutes.</center></html>";
 exit;
 } 
 
@@ -20,18 +23,46 @@ fclose($fp);
 // set  perms so script owner can delete after exec.
 chmod( $DelFile, 0777 );
 
-print "<center>Account queued to be disabled.</center>";
+print "<center><img src=\"dudi.png\" border=\"0\"><br><br><b>Account queued to be disabled.<br>Click <a href=index.php>HERE to return.</a><br /></center>";
 }
 	else
 	{
 	$script = getenv("SCRIPT_NAME");
 	?>
-	
-	<form action="<? echo "$script"; ?>" method="POST">
-	<input type="hidden" name="action" value="add">
-	<center>Enter account to disable:<br>
-	<input type="text" name="useracct" size="15">
-	<input type="submit" value="submit">
+	<center>
+	<table>
+		<tr>
+			<td rowspan="2"><img src="dudi.png" border="0"></td>
+			<td valign="bottom">	
+			<form action="<? echo "$script"; ?>" method="POST">
+			<input type="hidden" name="action" value="add">
+			Enter account to disable:
+			</td>
+		</tr>
+		<tr>
+			<td valign="top">
+			<input type="text" name="useracct" size="15">
+			<input type="submit" value="submit">
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2">
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center">
+				Last account disabled
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center">
+	<?
+			$LastAccts = file('/home/www-data/log');
+			print_r($LastAccts[0]);
+	?>
+			</td>
+		</tr>
+	</table>
 	</center>
 	<?
 	}
